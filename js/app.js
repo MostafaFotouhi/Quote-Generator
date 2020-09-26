f0febf490e66da7463669907a9d436f186326ef9
mostafa fotouhi <mfotouhi8@gmail.com>
2020-09-26 17:21:41 +0330

    js files

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const quoteContainer = document.getElementById("qoute-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQoutes = [];
+
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  if (!quote.author) {
+    quoteText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+async function getQuote() {
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {}
+}
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+getQuote();
+// newQuote();
